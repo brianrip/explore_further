5b29e9646ab3226af3b3fc6b0f9a33d49b6520a3
Brian Rippeto <rippeto@gmail.com>
2016-04-27 00:08:48 -0600

    Add database cleaner and finish favorite crag feature test

--- spec/features/athlete_views_favorite_crags_spec.rb
+++ spec/features/athlete_views_favorite_crags_spec.rb
@@ -2,19 +2,14 @@ require 'rails_helper'
 include Omnistub
 
 RSpec.feature 'Athlete views favorite crag' do
-  before(:each) do
-    stub_athlete
-  end
   scenario 'athlete sees views favorite crags from dashboard' do
     VCR.use_cassette 'activty#display_favorite_crag' do
-      crag = create(:crag)
-
+      athlete = stub_athlete
+      crag    = create(:crag, favorite: true, athlete_id: 6 )
       visit '/'
       find('#login').click
-
       click_on "View favorite climbing areas"
-
-      expect(page).to have_content
+      expect(page).to have_content("Young and the restless")
     end
   end
 end
