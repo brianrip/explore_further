f0d4cb41d00697ae41903f1ead392ea9bc255017
Brian Rippeto <rippeto@gmail.com>
2016-04-27 09:48:40 -0600

    Finish crag conrtoller clean up

--- app/controllers/crags_controller.rb
+++ app/controllers/crags_controller.rb
@@ -8,13 +8,8 @@ class CragsController < ApplicationController
     activity = Activity.find_by(id: params[:id])
     distance = 0.01
     athlete_id = current_athlete.id
-    CragsPresenter.build_crags(activity, distance, athlete_id)
+    CragsBuilder.build_crags(activity, distance, athlete_id)
 
-    # bounding_box = ActivityBox.build_box(activity, distance)
-    # area_node = CragService.new.climbing_area(bounding_box)
-    # crag_data = CragService.new.crag_children(area_node[:data][:mapto].first)
-    # crags = Crag.build_crag(crag_data)
-    # crags.each { |crag| crag.update(activity_id: activity.id, athlete_id: current_athlete.id) }
     redirect_to activity_path(activity.slug)
   end
 end
