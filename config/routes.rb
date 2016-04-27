35433187b7ad037b725a684c2dcbb40fb89d75e8
Brian Rippeto <rippeto@gmail.com>
2016-04-27 15:33:54 -0600

    Organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :activities, only: [:index, :show]
+
+  root 'landings#index'
+  get '/auth/strava',          as: :strava_login
+  get '/auth/strava/callback', to: "sessions#create"
+  delete '/logout',            to: "sessions#destroy"
+  get '/dashboard',            to: 'landings#show'
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
@@ -7,14 +12,7 @@ Rails.application.routes.draw do
     end
   end
 
-
+  resources :activities, only: [:index, :show]
   resources :crags, only: [:index, :create]
-
   resources :favorite_crags, only: [:index]
-
-  root 'landings#index'
-  get '/auth/strava', as: :strava_login
-  get '/auth/strava/callback', to: "sessions#create"
-  delete '/logout', to: "sessions#destroy"
-  get '/dashboard', to: 'landings#show'
 end
